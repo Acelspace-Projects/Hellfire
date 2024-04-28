3e0ba262b5b0fc8eabcbb4200d033e4ecdd60a80
Mixcraftio <dimitri.faj@gmail.com>
2024-04-28 17:32:29 +0200

    Add boards-related instructions

---
+++ Code/LoraFeather/LoraFeather.ino
@@ -0,0 +1,13 @@
+// M0 Lora Feather Code
+// v0.1
+// author : Mixcraftio
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
