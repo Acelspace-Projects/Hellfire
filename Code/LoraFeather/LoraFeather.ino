9cdbf6cf978d0c9465659f2692604e4d9b21347e
Mixcraftio <dimitri.faj@gmail.com>
2024-05-07 17:28:53 +0200

    zbeub

--- Code/LoraFeather/LoraFeather.ino
+++ Code/LoraFeather/LoraFeather.ino
@@ -1,13 +1,173 @@
-// M0 Lora Feather Code
+// M0 Lora Feather Code + LSM6DSOX sensor
 // v0.1
 // author : Mixcraftio
 
-void setup() {
-  // put your setup code here, to run once:
+#define DEBUG
+#define DELAY_MESURE 50 // µs
 
+#include <Adafruit_LSM6DSOX.h>
+
+Adafruit_LSM6DSOX sox;
+
+
+unsigned long previousMillis = 0;
+
+void setup(void) {
+  #ifdef DEBUG
+    Serial.begin(115200);
+    Serial.println("Initialising...");
+    while (!Serial);
+    Serial.println("Adafruit LSM6DSOX test!");
+  #endif
+  
+  sox.begin_I2C();
+  sox.setAccelRange(LSM6DS_ACCEL_RANGE_16_G);
+  sox.setGyroRange(LSM6DS_GYRO_RANGE_1000_DPS);
+  sox.setAccelDataRate(LSM6DS_RATE_6_66K_HZ);
+  sox.setGyroDataRate(LSM6DS_RATE_6_66K_HZ);
+
+  #ifdef DEBUG
+  Serial.println("LSM6DSOX initialised!");
+
+  Serial.print("Accelerometer range set to: ");
+  switch (sox.getAccelRange()) {
+  case LSM6DS_ACCEL_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case LSM6DS_ACCEL_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case LSM6DS_ACCEL_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case LSM6DS_ACCEL_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+
+  Serial.print("Gyro range set to: ");
+  switch (sox.getGyroRange()) {
+  case LSM6DS_GYRO_RANGE_125_DPS:
+    Serial.println("125 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_250_DPS:
+    Serial.println("250 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_500_DPS:
+    Serial.println("500 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_1000_DPS:
+    Serial.println("1000 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_2000_DPS:
+    Serial.println("2000 degrees/s");
+    break;
+  case ISM330DHCX_GYRO_RANGE_4000_DPS:
+    break; // unsupported range for the DSOX
+  }
+
+  Serial.print("Accelerometer data rate set to: ");
+  switch (sox.getAccelDataRate()) {
+  case LSM6DS_RATE_SHUTDOWN:
+    Serial.println("0 Hz");
+    break;
+  case LSM6DS_RATE_12_5_HZ:
+    Serial.println("12.5 Hz");
+    break;
+  case LSM6DS_RATE_26_HZ:
+    Serial.println("26 Hz");
+    break;
+  case LSM6DS_RATE_52_HZ:
+    Serial.println("52 Hz");
+    break;
+  case LSM6DS_RATE_104_HZ:
+    Serial.println("104 Hz");
+    break;
+  case LSM6DS_RATE_208_HZ:
+    Serial.println("208 Hz");
+    break;
+  case LSM6DS_RATE_416_HZ:
+    Serial.println("416 Hz");
+    break;
+  case LSM6DS_RATE_833_HZ:
+    Serial.println("833 Hz");
+    break;
+  case LSM6DS_RATE_1_66K_HZ:
+    Serial.println("1.66 KHz");
+    break;
+  case LSM6DS_RATE_3_33K_HZ:
+    Serial.println("3.33 KHz");
+    break;
+  case LSM6DS_RATE_6_66K_HZ:
+    Serial.println("6.66 KHz");
+    break;
+  }
+
+  Serial.print("Gyro data rate set to: ");
+  switch (sox.getGyroDataRate()) {
+  case LSM6DS_RATE_SHUTDOWN:
+    Serial.println("0 Hz");
+    break;
+  case LSM6DS_RATE_12_5_HZ:
+    Serial.println("12.5 Hz");
+    break;
+  case LSM6DS_RATE_26_HZ:
+    Serial.println("26 Hz");
+    break;
+  case LSM6DS_RATE_52_HZ:
+    Serial.println("52 Hz");
+    break;
+  case LSM6DS_RATE_104_HZ:
+    Serial.println("104 Hz");
+    break;
+  case LSM6DS_RATE_208_HZ:
+    Serial.println("208 Hz");
+    break;
+  case LSM6DS_RATE_416_HZ:
+    Serial.println("416 Hz");
+    break;
+  case LSM6DS_RATE_833_HZ:
+    Serial.println("833 Hz");
+    break;
+  case LSM6DS_RATE_1_66K_HZ:
+    Serial.println("1.66 KHz");
+    break;
+  case LSM6DS_RATE_3_33K_HZ:
+    Serial.println("3.33 KHz");
+    break;
+  case LSM6DS_RATE_6_66K_HZ:
+    Serial.println("6.66 KHz");
+    break;
+  }
+  #endif
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= DELAY_MESURE) {
+    sensors_event_t accel;
+    sensors_event_t gyro;
+    sensors_event_t temp;
+    sox.getEvent(&accel, &gyro, &temp);
+
+    #ifdef DEBUG
+    Serial.print(temp.temperature); Serial.print(","); // °C
+
+    Serial.print(accel.acceleration.x); Serial.print(","); // m.s^-2
+    Serial.print(accel.acceleration.y); Serial.print(","); // m.s^-2
+    Serial.print(accel.acceleration.z); Serial.print(","); // m.s^-2
+
+    Serial.print(gyro.gyro.x); Serial.print(","); // rad.s^-1
+    Serial.print(gyro.gyro.y); Serial.print(","); // rad.s^-1
+    Serial.print(gyro.gyro.z); Serial.println(); // rad.s^-1
+
+    delayMicroseconds(10000);
+    // #else
+    #endif
+
+    previousMillis = currentMillis;
+  }
 
 }
